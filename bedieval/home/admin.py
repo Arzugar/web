65dfb09ecc9bc0debdf1742f2583b3749e5809d5
Arzugar <lulu975@ouvaton.org>
2024-02-03 15:16:19 +0100

    changements de fichier

--- bedieval/home/admin.py
+++ bedieval/home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Member, Event
+
+admin.site.register(Member)
+admin.site.register(Event)
 
 # Register your models here.
--- bedieval/home/views.py
+++ bedieval/home/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Member, Event
 # Create your views here.
 
 
 def index(request):
-    return render(request, "home/index.html")
+    members = Member.objects.all()
+    events = Event.objects.all()
+    
+    context = {
+        'members': members,
+        'events': events,
+    }
+
+    return render(request, "home/index.html", context)
 
 
 def presentations(request):
