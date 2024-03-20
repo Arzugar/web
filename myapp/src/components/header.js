66b92937fce07212682293a61d80446b61e38eab
Arzugar <lulu975@ouvaton.org>
2024-03-21 00:43:34 +0100

    modifs

--- myapp/src/components/header.js
+++ myapp/src/components/header.js
@@ -9,18 +9,23 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <Navbar expand="lg" className="navbar-expand-lg navbar-collapse navbar-dark">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img alt="Ouais tkt" className='logo_img' src={logo}></img>
 
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" activeClassName="active" >
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/presentations">Présentations</Nav.Link>
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <Navbar expand="md" className="navbar-collapse navbar-dark">
+
+            <Navbar.Brand href="#h</Navbar>ome">
+                <img alt="Ouais tkt" className='logo_img' src={logo}></img>
+
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto" activeClassName="active" >
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/presentations">Présentations</Nav.Link>
+                    <Nav.Link as={Link} to="/">Truc</Nav.Link>
+                    <Nav.Link as={Link} to="/">Machin</Nav.Link>
+                    <Nav.Link as={Link} to="/">Jamais on a autant de pages</Nav.Link>
+
+                    {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
                                 Another action
@@ -31,10 +36,11 @@ function Header() {
                                 Separated link
                             </NavDropdown.Item>
                         </NavDropdown> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
+
+
     );
 
 }
--- myapp/src/pages/home.js
+++ myapp/src/pages/home.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Header from '../components/header'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
+import Line from '../components/line';
+
 
 function Home() {
     return (
         <>
-
             <Header />
+            <Container className="mt-5">
+                <div className="position-relative">
+                    <img className="big-bg-img img-fluid" src="" alt="background_image" /><img />
+                    <div id="text-over-bg" className="position-relative" >
+                        <h1 className="text-white">Bienvenue sur le site de la meilleure liste</h1>
+                        <h4 className="text-white">Et là c'est un sous-titre, genre une description un peu plus longue</h4>
+                        <Button className='btn-light btn-sm' href='/presentations'>
+                            Lire l'article
+                        </Button>
+                    </div>
+                </div>
+            </Container >
+            <Line />
         </>
-
     );
 }
 
