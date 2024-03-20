670e9065e27d8e6c8eeb44257cc9d4bf181c6323
Arzugar <lulu975@ouvaton.org>
2024-03-20 22:46:47 +0100

    ajout des fichier

--- myapp/src/components/header.js
+++ myapp/src/components/header.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import "./header.css";
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../images/Logo.png';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../images/logo_rect.png';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="#">
-                <img src={Logo} alt="" className="logo_img" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarNav" />
-            <Navbar.Collapse id="navbarNav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/home/presentations">Presentations</Nav.Link>
-                    <Nav.Link href="#">Other</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+        <Navbar expand="lg" className="navbar-expand-lg navbar-collapse navbar-dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img alt="Ouais tkt" className='logo_img' src={logo}></img>
+
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" activeClassName="active" >
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/presentations">Présentations</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown> */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
-}
 
+}
 
 export default Header;
--- myapp/src/pages/home.js
+++ myapp/src/pages/home.js
@@ -4,11 +4,8 @@ import Header from '../components/header'
 function Home() {
     return (
         <>
+
             <Header />
-            <div>
-                <h1>Home Page</h1>
-                <p>This is the home page.</p>
-            </div>
         </>
 
     );
