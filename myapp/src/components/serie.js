5f31f0241c7ea4f82776de01b8f3280cd99d8af9
Arzugar <lulu975@ouvaton.org>
2024-03-21 02:27:34 +0100

    revenu à un truc plus simple

--- myapp/src/components/serie.js
+++ myapp/src/components/serie.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import ReactDOM from 'react-dom';
 
 function Serie({ id }) {
     let [data, setData] = useState([]);
@@ -22,39 +23,16 @@ function Serie({ id }) {
         "content": "Contenu",
         "image": "https://via.placeholder.com/150"
     }
-    const renderOver = () => {
-        //console.log("renderOver");
-        return (
-
-            <Tooltip className="machin" id="button-tooltip-2">Check out this avatar</Tooltip>
-
-        );
-    }
-        ; // doit renvoyer l'overlay correspondant
-
     return (
-        <>
-            <OverlayTrigger
-
-                placement="top"
-                overlay={renderOver}>
-
-                {({ ref, ...triggerHandler }) => (
-                    <Card {...triggerHandler} className="bg-dark text-white mycard">
-                        <Card.Img src={data.image} alt="Image de la card" />
-                        <Card.ImgOverlay>
-                            <Card.Title ref={ref} > {data.Title}</Card.Title>
-                            <Card.Text>
-                                {data.content}
-                            </Card.Text>
-                        </Card.ImgOverlay>
-                    </Card>
-                )}
-
-
-            </OverlayTrigger>
-        </>
-    );
+        <Card className="bg-dark text-white mycard">
+            <Card.Img src={data.image} alt="Image de la card" />
+            <Card.ImgOverlay>
+                <Card.Title >{data.Title}</Card.Title>
+                <Card.Text>
+                    {data.content}
+                </Card.Text>
+            </Card.ImgOverlay>
+        </Card>);
 }
 
 export default Serie;
