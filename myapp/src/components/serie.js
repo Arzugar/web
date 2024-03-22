5aaaf406adaab8ca0f543a030b96f1d82b3b79e0
Arzugar <lulu975@ouvaton.org>
2024-03-22 20:01:34 +0100

    plug le back (en partie)

--- myapp/src/components/serie.js
+++ myapp/src/components/serie.js
@@ -10,19 +10,26 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import PH from '../images/Infographie.png'
 import PlayButton from "../images/play-button.png"
+import { Link } from 'react-router-dom';
 
 function CustomModal({ show, onClose, children, data }) {
+    function unshow() {
+        document.body.classList.remove('modal-open');
+    }
+
     useEffect(() => {
         if (show) {
             document.body.classList.add('modal-open');
         } else {
-            document.body.classList.remove('modal-open');
+            unshow();
         }
     }, [show]);
 
     if (!show) {
         return null;
     }
+
+
 
 
     const stopPropagation = (e) => {
@@ -34,13 +41,13 @@ function CustomModal({ show, onClose, children, data }) {
             <div className="custom-modal-content" onClick={stopPropagation}>
                 <Button className='close-button' onClick={onClose}>x</Button>
                 <div className='big-image-container'>
-                    <img className='ZoomedImage' src={PH} alt="image de la série" />
+                    <img className='ZoomedImage' src={data.image} alt="Bigpic" />
                     <div className='serie-title text-primary'>
-                        <h1>Pole infographie</h1>
-                        <Button className='btn-light'>
+                        <h1>data.title</h1>
+                        <Button as={Link} to='/construction' className='btn-light' onClick={unshow}>
                             <img className='PlayButtonImage' src={PlayButton} alt="Ne mêne à rien" />
 
-                            Vers la vidéo ?
+                            Mystère ...
                         </Button>
                     </div>
                 </div>
@@ -57,10 +64,10 @@ function CustomModal({ show, onClose, children, data }) {
 
                             </div>
                             <div className="col-4">
-                                <span className='casting-title'>
-                                    Casting :
+                                <span className='info-title'>
+                                    {data.info_2_title}
                                 </span>
-                                <span >{data.casting}</span>
+                                <span >{data.info_2}</span>
                             </div>
                         </div>
                         <div className="row">
@@ -72,10 +79,10 @@ function CustomModal({ show, onClose, children, data }) {
 
                             </div>
                             <div className="col-4">
-                                <span className='casting-title'>
-                                    Genre :
+                                <span className='info-title'>
+                                    {data.info_2_title}
                                 </span>
-                                <span >{data.genre}</span>
+                                <span >{data.info_2}</span>
 
                             </div>
                         </div>
@@ -88,11 +95,11 @@ function CustomModal({ show, onClose, children, data }) {
     );
 }
 
-function Serie({ id }) {
+function Serie({ type, id }) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(`/api/series/${id}`)
+        fetch(`/${type}/${id}`)
             .then(response => response.json())
             .then(data => setData(data))
             .catch(error => {
@@ -102,7 +109,14 @@ function Serie({ id }) {
                 setData({
                     "Title": "Titre par défaut",
                     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.",
-                    "image": ''
+                    "image": 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F149367133.v2.pressablecdn.com%2Fwp-content%2Fuploads%2F2021%2F02%2Frick-astley-never-gonna-give-you-up-4k.jpg&f=1&nofb=1&ipt=a23aa21be1e1262ce10d68d5ba6817f9c4390d6b3565a289d9e79c4f50a01ae4&ipo=images',
+                    "rating": 100,
+                    "year": 2021,
+                    "info_1_title": "Info 1 titre",
+                    "info_1": "Info 1",
+                    "info_2_title": "Info 2 titre",
+                    "info_2": "Info 2",
+
                 });
             });
     }, [id]);
@@ -114,11 +128,10 @@ function Serie({ id }) {
 
     return (
         <>
-            <Image src={PH} alt="image de la série" className="BaseImage" onClick={handleShow} />
+            <Image src={data.image} alt="pic" className="BaseImage" onClick={handleShow} />
             <CustomModal className='' show={show} onClose={handleClose} data={data}>
 
 
-                {/* Ajoutez ici d'autres informations que vous voulez afficher dans la fenêtre contextuelle */}
             </CustomModal >
         </>
     );
--- myapp/src/pages/construction.js
+++ myapp/src/pages/construction.js
@@ -5,9 +5,9 @@ const ConstructionPage = () => {
     return (
         <div>
             <Header />
-            <Container fluid>
-                <h1>Site en construction</h1>
-                <img src="chemin_vers_votre_image" alt="Image de construction" />
+            <Container>
+                <h1 className='text-primary'>Site en construction</h1>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.leparisien.fr%2Fresizer%2FVfey6LpteAkXt-wgnOOROMGh86g%3D%2F1280x672%2Farc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com%2Fpublic%2FTWXDWP66U7UTLKC5LMVYOWS2Z4.jpg&f=1&nofb=1&ipt=f64c0380dc6294970b9b328dcd1a76cd16948c3aac3c2a47acd1b4b6f84a3800&ipo=images" alt="Image de construction" />
             </Container>
         </div >
     );
