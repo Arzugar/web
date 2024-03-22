1c9f21da506930ada27d33b207e9645b6cb569fd
Arzugar <lulu975@ouvaton.org>
2024-03-22 21:27:58 +0100

    debug

--- bedflix_website/main/views.py
+++ bedflix_website/main/views.py
@@ -51,6 +51,7 @@ def get_evenement(_, id):
 
 
 def get_image(_, path):
-    with open(path, 'rb') as f:
+    print("appel à get image")
+    with open("images/" + path, 'rb') as f:
         image_data = f.read()
     return HttpResponse(image_data, content_type='image/png')
