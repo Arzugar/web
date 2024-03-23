d3f755a4f2d95878fd02da4dfcbf66ef91c9d8ff
Arzugar <lulu975@ouvaton.org>
2024-03-23 15:53:08 +0100

    debug

--- bedflix_website/bedflix_website/settings.py
+++ bedflix_website/bedflix_website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)$gedqxxl)-1uk7v*c50jq4%d31dy^1d3ur-u!gn_5^!tl#$s2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["bedflix.fr", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["bedflix.fr", "127.0.0.1", "localhost", "137.194.13.155"]
 
 
 # Application definition
--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-ln --symbolic serv.conf /etc/nginx/sites-enabled/serv.conf
 
 cd myapp
 npm run build
 cd ../beflix_website
 python3 manage.py collectstatic
-gunicorn bedflix_website.wsgi -D
+gunicorn --bind unix:/tmp/gunicorn.sock bedflix_website.wsgi -D
 systemctl restart nginx
